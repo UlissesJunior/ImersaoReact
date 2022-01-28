2f9e252a99104b5737d65f8644800a022e0af7b8
UlissesJunior <ulissesjunior2005@gmail.com>
2022-01-28 16:27:47 -0300

    href

--- src/components/chat.js
+++ src/components/chat.js
@@ -2,16 +2,13 @@ import React from "react";
 import ReactLogo from "../img/react_logo.png";
 
 export default function PageChat() {
-  const back = () => {
-    window.location.href='/ImersaoReact/';
-}
   return (
     <>
       <div className="container-chat">
         <div className="nav">
           <a>Chat React</a>
           <img src={ReactLogo} alt="React Logo" />
-          <a onClick={back}>Logout</a>
+          <a>Logout</a>
         </div>
       </div>
     </>
