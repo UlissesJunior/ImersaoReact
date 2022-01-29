6ab4f36d54b217247529e9783b8b18aef59b15db
UlissesJunior <ulissesjunior2005@gmail.com>
2022-01-29 17:09:26 -0300

    User Not Found

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -9,7 +9,7 @@ const Profile = ({ Github }) => {
             <img src={Github.avatar_url} alt='user'/>
           </div>
           <div className='img-content'>
-              <h3>{Github.login}</h3>
+              <h3>{Github.login || 'User Not Found'}</h3>
               <h2>{Github.followers} Followers</h2>
               <h2>{Github.following} Following</h2>
               <h2>{Github.public_repos} Repositories</h2>
