3ea312191e4a03f4062d37218adb1ab5d1e31a21
UlissesJunior <ulissesjunior2005@gmail.com>
2022-01-30 20:07:16 -0300

    Comment Useeffect

--- src/chat.js
+++ src/chat.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 // import { createClient } from "@supabase/supabase-js";
