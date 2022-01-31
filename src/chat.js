7eb51c3b3297d675ab07ee18fe75086b758c9443
UlissesJunior <ulissesjunior2005@gmail.com>
2022-01-31 10:07:37 -0300

    hook

--- src/chat.js
+++ src/chat.js
@@ -29,7 +29,7 @@ export default function PageChat({ Github }) {
         console.log("Dados da consulta:", data);
         setMessagelist(data);
       });
-  }, []);
+  }, [messagelist]);
 
   const TextType = (event) => {
     const value = event.target.value;
