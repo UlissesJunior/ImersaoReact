ff9e445076a8a851339ccb83aa9548469286bff5
UlissesJunior <ulissesjunior2005@gmail.com>
2022-08-18 08:33:54 -0300

    fix the useEffect unnecessary

--- src/chat.js
+++ src/chat.js
@@ -29,7 +29,7 @@ export default function PageChat({ Github }) {
       .then(({ data }) => {
         setMessagelist(data);
       });
-  }, [supabase]);
+  }, []);
 
   const TextType = (event) => {
     const value = event.target.value;
